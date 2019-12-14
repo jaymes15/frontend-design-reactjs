e9791bdd3916620fc2d5e210087dbd3f65dbe8f5
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2019-12-13 20:00:53 -0800

    Updates

---
+++ precache-manifest.373ea3efa411aabdaab27acc2467d2a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df5bf7ec840bd88fabf1a89b8c00138",
+    "url": "/frontend-design-reactjs/index.html"
+  },
+  {
+    "revision": "ab1dd26fe0e4314fa34d",
+    "url": "/frontend-design-reactjs/static/css/main.7eed866b.chunk.css"
+  },
+  {
+    "revision": "f75e7618ee2622b2bb1b",
+    "url": "/frontend-design-reactjs/static/js/2.55031c84.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/frontend-design-reactjs/static/js/2.55031c84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab1dd26fe0e4314fa34d",
+    "url": "/frontend-design-reactjs/static/js/main.b751b844.chunk.js"
+  },
+  {
+    "revision": "ce195b35bb0e60fcd281",
+    "url": "/frontend-design-reactjs/static/js/runtime-main.aaba81ab.js"
+  }
+]);
